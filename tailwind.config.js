4a67ddda2e883107028da79a3e1dbb366f49884a
Obey <obeyblessing@gmail.com>
2025-07-16 23:24:07 +0200

    Update: hero content updated

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ["./src/**/*.{html,js}"], // adjust to your source files
-    theme: {
-        extend: {},
-    },
-    plugins: [],
-}
