817447ddad1c89fe1328de519325f072ef71fbee
Obey <obeyblessing@gmail.com>
2025-07-28 00:53:45 +0200

    Fix: /

--- src/main/java/xyz/cashbreaker/break_down_maker/web/HomeController.java
+++ src/main/java/xyz/cashbreaker/break_down_maker/web/HomeController.java
@@ -2,6 +2,7 @@ package xyz.cashbreaker.break_down_maker.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
@@ -11,4 +12,11 @@ public class HomeController {
         return "index"; // assumes index.html is in src/main/resources/templates/
 
     }
+
+    @GetMapping("/test")
+    @ResponseBody
+    public String test() {
+        return "Hello, world!";
+    }
+
 }
