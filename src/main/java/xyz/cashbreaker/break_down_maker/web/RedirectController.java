33b3e05c6bd95b81c3881916bf094fa873ce46d2
Obey <obeyblessing@gmail.com>
2025-07-27 22:35:28 +0200

    Fix: redirect controller

---
+++ src/main/java/xyz/cashbreaker/break_down_maker/web/RedirectController.java
@@ -0,0 +1,33 @@
+package xyz.cashbreaker.break_down_maker.web;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+import java.io.IOException;
+
+@Controller
+public class RedirectController {
+
+    @RequestMapping("/**")
+    public void redirectBasedOnDomain(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        String host = request.getHeader("host");
+
+        if (host != null && host.contains("cashbreaker.sizafuel.xyz")) {
+            String path = request.getRequestURI();
+            String query = request.getQueryString();
+            String targetUrl = "https://www.breakmycash.online" + path;
+            if (query != null) {
+                targetUrl += "?" + query;
+            }
+
+            response.setStatus(HttpServletResponse.SC_MOVED_PERMANENTLY); // 301 redirect
+            response.setHeader("Location", targetUrl);
+        } else {
+            request.getRequestDispatcher(request.getRequestURI()).forward(request, response);
+        }
+    }
+}
